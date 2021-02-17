b881e9d1e987ffd392542b547e56f8687a660b34
mapol2000 <20909git>
2021-02-17 17:36:31 +0900

    Initial commit

---
+++ app/src/main/java/mapol2000/hellolayout/LinearActivity.kt
@@ -0,0 +1,11 @@
+package mapol2000.hellolayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear)
+    }
+}
